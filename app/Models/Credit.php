a10315ea0956a24a9a739cd7b95d35dd74a758f9
David Lun <mysticcode@gmail.com>
2021-02-05 19:00:50 +0200

    Covers all possible overlaps

--- app/Models/Credit.php
+++ app/Models/Credit.php
@@ -11,7 +11,17 @@ class Credit extends Model
 
     public function scopeValidForRange($query, array $range = [])
     {
-        return $query->whereBetween('from', $range)->orWhereBetween('to', $range);
+        return $query
+            ->where(function ($query) use ($range) {
+                $query->where('from', '>=', reset($range))->where('to', '<=', end($range));
+            })
+            ->orWhere(function ($query) use ($range) {
+                $query->whereBetween('from', $range)->orWhereBetween('to', $range);
+            })
+            ->orWhere(function ($query) use ($range) {
+                $query->where('from', '<=', reset($range))
+                    ->where('to', '>=', end($range));
+            });
     }
 
     public function scopeValidForDate($query, string $date)
