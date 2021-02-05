f274aaa101e9d323cae5a9d99902df518956aa13
David Lun <mysticcode@gmail.com>
2021-02-05 17:42:11 +0200

    both cases

--- app/Models/Credit.php
+++ app/Models/Credit.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Credit extends Model
 {
     use HasFactory;
+
+    public function scopeValidSince($query, string $from)
+    {
+        return $query->where('from', '>=', $from);
+    }
+
+    public function scopeValidUntil($query, string $to)
+    {
+        return $query->where('to', '<=', $to);
+    }
+
+    public function scopeValidBetween($query, array $range = [])
+    {
+        return $query->validSince(reset($range))->validUntil(end($range));
+    }
+
+    public function scopeValidInBetween($query, string $date)
+    {
+        return $query->where('from', '<=', $date)->where('to', '>=', $date);
+    }
 }
--- test.php
+++ test.php
@@ -3,23 +3,10 @@
 use App\Models\Credit;
 
 $case1 = ['2021-02-01 00:00:00', '2021-02-28 23:59:59'];
-$case2 = ['2021-02-05 12:30:00', '2021-02-05 12:30:00'];
+$res1  = Credit::validBetween($case1);
 
-function checkDataset(string $from, string $to)
-{
-    $credits = Credit::query();
-
-    $credits->when($from, function ($q, $from) {
-        return $q->where('from', '>=', $from);
-    })->when($to, function ($q, $to) {
-        return $q->where('to', '<=', $to);
-    });
-
-    return $credits->get();
-}
-
-$res1 = checkDataset($case1[0], $case1[1]);
-$res2 = checkDataset($case1[0], $case1[1]);
+$case2 = '2021-02-05 12:30:00';
+$res2  = Credit::validInBetween($case2);
 
 echo $res1->count() . "\n";
 echo $res2->count() . "\n";
