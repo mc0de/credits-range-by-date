4dc6b05dfff0b6f9db48e5e98c1cc5082b792fca
David Lun <mysticcode@gmail.com>
2021-02-05 19:03:16 +0200

    adds comments

--- app/Models/Credit.php
+++ app/Models/Credit.php
@@ -12,12 +12,15 @@ class Credit extends Model
     public function scopeValidForRange($query, array $range = [])
     {
         return $query
+            // Covers outer bounds
             ->where(function ($query) use ($range) {
                 $query->where('from', '>=', reset($range))->where('to', '<=', end($range));
             })
+            // Covers left and right bound
             ->orWhere(function ($query) use ($range) {
                 $query->whereBetween('from', $range)->orWhereBetween('to', $range);
             })
+            // Covers inner bounds
             ->orWhere(function ($query) use ($range) {
                 $query->where('from', '<=', reset($range))
                     ->where('to', '>=', end($range));
